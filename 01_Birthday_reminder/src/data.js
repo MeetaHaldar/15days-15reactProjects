b10c2964d6018f170259ea818f7c51d4bb67d8ff
meeta haldar <meetahaldar1001@gmail.com>
2022-09-13 11:51:23 +0530

    completed

---
+++ 01_Birthday_reminder/src/data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    id: 1,
+    name: "Kanak",
+    age: 20,
+    DOB :"12-jan-2002",
+    image:
+      "https://pbs.twimg.com/profile_images/1523978720213569536/8ZumfYzg_400x400.jpg",
+  },
+  {
+    id: 2,
+    name: "Aastha chaudhary",
+    age: 22,
+    DOB :"18-April-2000",
+    image:
+      "https://media.baamboozle.com/uploads/images/11420/1615231931_58740.jpeg",
+  },
+  {
+    id: 3,
+    name: "Tarang Haldar",
+    age: 19,
+    DOB :"19-june-2003",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Laxmi Singh",
+    age: 20,
+    DOB :"25-oct-2001",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Anjali",
+    age: "21",
+    DOB :"16-feb-2001",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
+
+export default data;

cfb282525d802afbc1a6e88e480ca1ab68dd7736
meeta haldar <meetahaldar1001@gmail.com>
2022-09-17 14:23:34 +0530

    Reviews completed

--- 02_Tours/src/App.js
+++ 02_Tours/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Loadings from "./Loading";
 import Tours from "./Tours";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
